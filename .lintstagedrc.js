1c30d27274b255443a9662003c05cf73b590c23a
Shubham Naik <shub@memgpt.ai>
2025-03-04 10:05:10 -0800

    chore: next

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,6 @@
 module.exports = {
   '*': ['nx format:write --files'],
-  // '**/src/**/*.{js,jsx,ts,tsx}': allStagedFiles => {
-  //     console.log('a', allStagedFiles)
-  //     return [...allStagedFiles]
-  //         .filter(([project, files]) => files.length)
-  //         .map(([projectName, files]) => `nx lint ${projectName} --lintFilePatterns=${files.join(',')}`);
-  // }
+  '**/src/**/*.{js,jsx,ts,tsx}': (allStagedFiles) => {
+    return `nx lint ${allStagedFiles.join(' ')}`;
+  },
 };
