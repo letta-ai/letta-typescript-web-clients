0118448b0e3051c65d9b482f9a478528dfd06128
Shubham Naik <shub@memgpt.ai>
2025-03-04 20:59:14 -0800

    chore: next

--- packages/letta-nextjs/jest.config.ts
+++ packages/letta-nextjs/jest.config.ts
@@ -1,6 +1,5 @@
 export default {
   displayName: 'letta-nextjs',
-  preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
